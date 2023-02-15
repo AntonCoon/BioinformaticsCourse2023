e2f83f5a41c9d654ee5e2950c2b1193cc98f746e
jmkostina <113294009+jmkostina@users.noreply.github.com>
2023-02-16 00:21:28 +0300

    Update HW2_Task_2.py

--- HomeTask2/HW2_Task_2.py
+++ HomeTask2/HW2_Task_2.py
@@ -29,8 +29,8 @@ def gotoh_algo(seq1: str, seq2: str, alpha: float, beta: float):
 
     for i in range(1, len_seq1 + 1):
         for j in range(1, len_seq2 + 1):
-            A[i][j] = max(A[i-1][j] + beta, F[i-1][j] + (alpha+beta))
-            B[i][j] = max(B[i][j-1] + beta, F[i][j-1] + (alpha+beta))
+            A[i][j] = max(A[i-1][j] + beta, F[i-1][j] + alpha)
+            B[i][j] = max(B[i][j-1] + beta, F[i][j-1] + alpha)
             Match = F[i - 1][j - 1] + matrix[seq1[i - 1]][seq2[j - 1]]
             F[i][j] = max(Match, A[i][j], B[i][j])
 
@@ -40,18 +40,18 @@ def gotoh_algo(seq1: str, seq2: str, alpha: float, beta: float):
     r_seq1, r_seq2 = [], []
     while i > 0 or j > 0:
 
-        if i > 0 and j > 0 and F[i][j] == F[i - 1][j - 1] + matrix[seq1[i - 1]][seq2[j - 1]]:
-            r_seq1.append(seq1[i - 1])
-            r_seq2.append(seq2[j - 1])
-            i -= 1
-            j -= 1
-        elif i > 0 and (F[i][j] == max(A[i-1][j] + beta, F[i-1][j] + (alpha+beta))):
+        if i > 0 and F[i][j] == max(A[i][j], F[i-1][j] + alpha):
             r_seq1.append(seq1[i - 1])
             r_seq2.append('-')
             i -= 1
-        else:
+        elif j > 0 and F[i][j] == max(B[i][j], F[i][j-1] + alpha):
             r_seq1.append('-')
             r_seq2.append(seq2[j - 1])
+            j -= 1
+        else:
+            r_seq1.append(seq1[i - 1])
+            r_seq2.append(seq2[j - 1])
+            i -= 1
             j -= 1
     # Reverse the strings.
     r_seq1 = ''.join(r_seq1)[::-1]
