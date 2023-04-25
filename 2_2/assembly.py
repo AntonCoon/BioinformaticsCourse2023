7769824b24cb3c6ff5efe1639ac355d34c1ca64b
alex-shishkin <alex-shishkin@yandex-team.ru>
2023-04-25 11:26:02 +0300

    HW2_2: Refactor

--- 2_2/assembly.py
+++ 2_2/assembly.py
@@ -5,18 +5,18 @@ from typing import List
 # DSU with O(1) for operation
 class DSU:
     def __init__(self):
-        self.parent = []
-        self.sizes = []
+        self.parent: List[int] = []
+        self.sizes: List[int] = []
 
     def add_class(self):
         new_idx = len(self.parent)
         self.parent.append(new_idx)
         self.sizes.append(1)
 
-    def is_merged(self, x, y) -> bool:
+    def is_merged(self, x: int, y: int) -> bool:
         return self.get_class(x) == self.get_class(y)
 
-    def merge_classes(self, x, y) -> int:
+    def merge_classes(self, x: int, y: int) -> int:
         class_x = self.get_class(x)
         class_y = self.get_class(y)
         if class_x == class_y:
@@ -27,14 +27,14 @@ class DSU:
         self.sizes[class_x] += self.sizes[class_y]
         return class_x
 
-    def get_class(self, x):
+    def get_class(self, x: int):
         if self.parent[x] != x:
             self.parent[x] = self.get_class(self.parent[x])
         return self.parent[x]
 
 
 class GraphEdge:
-    def __init__(self, source, dest, weight):
+    def __init__(self, source: int, dest: int, weight: int):
         self.source = source
         self.dest = dest
         self.weight = weight
@@ -45,7 +45,7 @@ class GraphEdge:
 
 
 class GraphNode:
-    def __init__(self, id, text):
+    def __init__(self, id: int, text: str):
         self.id = id
         self.text = text
         self.can_be_left = True
@@ -62,18 +62,19 @@ class Graph:
     def __init__(self):
         self.nodes: List[GraphNode] = []
         # DSU keeps info about merged vertexes
+        # Assembly of union is in the parent node
         self.dsu = DSU()
         # Heap of edges with the most large overlap at top
         self.edges_heap: List[GraphEdge] = []
 
-    def create_node(self, s):
+    def create_node(self, s: str):
         idx = len(self.nodes)
         node = GraphNode(idx, s)
         self.nodes.append(node)
         self.dsu.add_class()
         print(f"Vertex {idx} with text <{s}> is created")
 
-    def add_edge(self, source, dest, weight):
+    def add_edge(self, source: int, dest: int, weight: int):
         edge = GraphEdge(source, dest, weight)
         heapq.heappush(self.edges_heap, edge)
         print(f"Edge {source} -> {dest} with weight {weight} is created")
@@ -102,7 +103,7 @@ class Graph:
         self._merge_vertexes(largest_edge)
         return True
 
-    # O(l)
+    # O(string concatenation)
     def _merge_vertexes(self, edge: GraphEdge):
         left_parent = self.nodes[self.dsu.get_class(edge.source)]
         right_parent = self.nodes[self.dsu.get_class(edge.dest)]
@@ -131,7 +132,7 @@ def z_function(s) -> List[int]:
     return res
 
 
-def build_graph(reads: List[str]) -> Graph:
+def build_graph(reads: List[str], threshold: int) -> Graph:
     graph = Graph()
     for read in reads:
         graph.create_node(read)
@@ -145,15 +146,15 @@ def build_graph(reads: List[str]) -> Graph:
             for k in range(1, min(len(reads[i]), len(reads[j]))):
                 if z_val[-k] == k:
                     common_length = k
-            if common_length > 0:
+            if common_length >= threshold:
                 graph.add_edge(j, i, common_length)
     return graph
 
 
-def compute_assembly(reads: List[str]):
+def compute_assembly(reads: List[str], threshold: int) -> str:
     # O(n^2 * l)
-    graph = build_graph(reads)
-    # Summary O(E log E + E * l) = O(n^2 log n + n^2 * l)
+    graph = build_graph(reads, threshold)
+    # Summary O(E * (log E + string concatenation))
     while graph.merge_by_largest_edge():
         pass
     active_nodes = [node.text for node in graph.get_active_nodes()]
@@ -162,12 +163,12 @@ def compute_assembly(reads: List[str]):
     return concatenated_text
 
 
-def test(input):
+def test(input: List[str], threshold: int):
     print(f"Test input: {input}")
-    print(f"Test output: {compute_assembly(input)}")
+    print(f"Test output: {compute_assembly(input, threshold)}")
     print()
 
 
 if __name__ == '__main__':
-    test(["AAA", "AAT", "ATT", "TTA", "TTT"])
-    test(["AAA", "CTA", "CCC", "TTA"])
+    test(["AAA", "AAT", "ATT", "TTA", "TTT"], 1)
+    test(["AAA", "CTA", "CCC", "TTA"], 1)
