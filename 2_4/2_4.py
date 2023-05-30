39c00221797fbad0bbb6842fa8438b7893d5a34f
Olga Pushkina <olya.pushkina.2001@gmail.com>
2023-05-30 23:08:27 +0300

    Pushkina 2_4

---
+++ 2_4/2_4.py
@@ -0,0 +1,23 @@
+from Bio import Phylo
+import matplotlib.pyplot as plt
+
+def fitch(tree):
+    nodes = tree.find_clades(order='postorder')
+    for node in nodes:
+        if not node.is_terminal():
+            children = node.clades
+            intersection = set.intersection(*[set(child.name) for child in children])
+            if intersection:
+                node.name = ''.join(intersection)
+            else:
+                union = set.union(*[set(child.name) for child in children])
+                node.name = ''.join(union)
+    return tree
+
+tree = Phylo.read('tree.newick', 'newick')
+tree = fitch(tree)
+Phylo.write(tree, 'output.newick', 'newick')
+#Phylo.draw_ascii(tree)
+
+Phylo.draw(tree, label_func=lambda x: x.name if x.name else '')
+plt.show()
